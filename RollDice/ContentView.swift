587e6542b14025430df7432cc1fb5e90bdb663a0
reshetnikd <dmitry.reshetnik@icloud.com>
2020-04-08 22:07:26 +0300

    Implement basic app behaviour

--- RollDice/ContentView.swift
+++ RollDice/ContentView.swift
@@ -10,27 +10,63 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var selection = 0
+    @State private var diceRolls = [String]()
+    @State private var randomIndex = 0
+    
+    let diceNumbers = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"]
  
     var body: some View {
         TabView(selection: $selection){
-            Text("First View")
-                .font(.title)
-                .tabItem {
-                    VStack {
-                        Image("first")
-                        Text("First")
-                    }
+            Button(action: {
+                self.randomIndex = Int.random(in: 0...self.diceNumbers.count - 1)
+                self.roll()
+            }) {
+                Text(diceNumbers[randomIndex])
+                    .font(.system(size: 164))
+                    .foregroundColor(Color.black)
+            }
+            .tabItem {
+                VStack {
+                    Image("first")
+                    Text("Throw")
                 }
-                .tag(0)
-            Text("Second View")
-                .font(.title)
-                .tabItem {
-                    VStack {
-                        Image("second")
-                        Text("Second")
-                    }
+            }
+            .tag(0)
+            List {
+                ForEach(diceRolls, id: \.self) { item in
+                    Text(item)
                 }
-                .tag(1)
+            }
+            .tabItem {
+                VStack {
+                    Image("second")
+                    Text("History")
+                }
+            }
+            .tag(1)
+        }
+        .onAppear(perform: {
+            self.loadData()
+            self.roll()
+        })
+    }
+    
+    func roll() {
+        diceRolls.append("\(diceNumbers[randomIndex]) rolled \(randomIndex + 1)")
+        saveData()
+    }
+    
+    func loadData() {
+        if let data = UserDefaults.standard.data(forKey: "Rolls") {
+            if let decoded = try? JSONDecoder().decode([String].self, from: data) {
+                self.diceRolls = decoded
+            }
+        }
+    }
+    
+    func saveData() {
+        if let data = try? JSONEncoder().encode(diceRolls) {
+            UserDefaults.standard.set(data, forKey: "Rolls")
         }
     }
 }
